1cd45f8fe6b11f2dac8d1fdf5abf2fe72192fcd1
GreenRamSilgen <kiranshrs@yahoo.com>
2026-02-03 10:31:55 -0800

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Kiran Shrestha on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
